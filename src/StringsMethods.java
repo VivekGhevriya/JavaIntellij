18d9a9ea6be6f6c8b639e5bfc51f639a6317b214
vivekghevariya <vivekghevariya83@gmal.com>
2021-05-17 22:34:51 -0400

    JavaMethods

--- src/StringsMethods.java
+++ src/StringsMethods.java
@@ -1,6 +1,39 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
+
 public class StringsMethods {
     public static void main(String[] args) {
-        String names ="Viveks";
-        System.out.print(names.length());
+//
+//
+//            equals() Method
+//        String names = "vivek";
+//        String name1 = "Vivek";
+//        System.out.println(names.equals(name1));
+
+//            trim() Method
+//        String trimExample = "  Hello World   ";
+//        System.out.println(trimExample);
+//        System.out.println(trimExample.trim());
+
+//              charAt() Method
+//        String name="Vivek";
+//        System.out.println(name.charAt(2));
+
+
+//          compareTo()
+//        String name1="vivek";
+//        String name2="Vivek";
+//        System.out.println(name1.compareTo(name2));
+
+//           contains()
+//        String name1="Java is the fun coding";
+//        System.out.println(name1.contains("Java"));
+
+
+//                startsWith()  & endswith()
+
+        String name1="I am learning Java";
+        System.out.println(name1.startsWith("I",0));
+        System.out.println(name1.endsWith("l"));
     }
 }
